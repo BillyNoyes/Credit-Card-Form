60ec6d8acb3cee4a2ee5c60d8da5156d992e96ef
William (Billy) Noyes <wbillypn@gmail.com>
2018-11-20 01:12:53 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//----- INSPIRATION & HTML MARKUP BY codepen.io/AlexKP/
+
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+        $("input").val("");
+        $("select").val("");
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
